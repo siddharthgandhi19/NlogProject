5d3d752f3be65ad0804570efaab2fcbf9970b50d
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-26 12:39:33 +0530

    [Siddharth]Add:Help us to Trace - Error, Info, Warn, Debug

---
+++ NLogProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace NLogProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition Problem \n Enter a,b values");
+            AddNumbers add = new AddNumbers();
+            Console.WriteLine("Result is" + add.Sum(0, 1));
+        }
+    }
+}
